b2a5b911b2e65a0ddc54087d052b9a35c88a3b6b
Alexey Kirienko <alkirienkoeps1@gmail.com>
2023-05-25 23:42:35 +0300

    Initial commit

---
+++ src/main/java/ru/kata/spring/boot_security/demo/entitys/User.java
@@ -0,0 +1,10 @@
+package ru.kata.spring.boot_security.demo.model;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+public class User {
+    }
+
